fd450b418025278f03c3bc842b6eaa93aeada562
Nicolás Varas <Nicolas Varas>
2021-04-07 14:04:10 -0400

    Deleted unused serializer

--- lunch_menu/serializers.py
+++ lunch_menu/serializers.py
@@ -6,17 +6,6 @@ from rest_framework import serializers
 from lunch_menu.models import Menu, Choice, EmployeeMenuChoice
 
 
-class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(
-        max_length=100,
-        style={'placeholder': 'Email', 'autofocus': True}
-    )
-    password = serializers.CharField(
-        max_length=100,
-        style={'input_type': 'password', 'placeholder': 'Password'}
-    )
-
-
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
@@ -29,12 +18,10 @@ class ChoiceSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
 class EmployeeMenuChoiceSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = EmployeeMenuChoice
-        fields = ('employee', 'choice','customization')
+        fields = ('employee', 'choice', 'customization')
         read_only_fields = ('menu', 'employee')
 
     def validate(self, data):
